78bc49e396a01c9424bea1c393c7216a01514400
James Arnold <j1mesu@hotmail.co.uk>
2021-10-14 13:59:40 +0100

    added npm inquirer

--- test.js
+++ test.js
@@ -20,6 +20,7 @@ const bone = 0
 const carrot = 0
 const catFood = 0
 
+
 // Animal classes
 class Animal {
   constructor(name) {
@@ -32,6 +33,10 @@ class Animal {
     // get - return allows us to see when console log.
   }
   get name() {
+    return this._name;
+  }
+
+  get hunger() {
     return this._hunger;
   }
 
@@ -44,7 +49,7 @@ class Animal {
   }
 
   get sleepy() {
-    return this.sleepy;
+    return this._sleepy;
   }
 
   // underneath here is class functions
@@ -73,7 +78,6 @@ class Animal {
     this._hunger + 10;
     this._thirst + 10;
     this._bored + 10;
-
   }
 
 }
@@ -109,60 +113,81 @@ class Dog extends Animal {
   }
 }
 
+if (Bunny.lovesCarrot == true) {
+  console.log(`${Animal.name} loved that carrot!`);
+  Animal.bored - 50;
+  Animal.hunger - 50;
+  Animal.thirst - 50;
+  Animal.sleepy - 50;
+} else {
+console.log("hello")
+}
+
+const Rosie = new Bunny("Rosie", true);
+
+//GET STATS FUNCTION
+const getStats = function(obj){
+  let stats = [];
+  for(let stat in obj){
+     stats.push(stat);
+  }
+  return stats;
+}
+
 //RANDOM EVENT AT START OF DAY FUNCTION
 function randomEvent () {
 let randomProb = Math.floor(Math.random() * 10);
 if (randomProb == 1) {
-  bone += 1;
   console.log(`${Animal._name} found a bone!`);
-} if else (randomProb == 2) {
-  carrot += 1;
+  bone += 1;
+} else if (randomProb == 2) {
   console.log(`${Animal._name} found a carrot!`);
-} if else (randomProb == 3) {
-  catFood += 1;
+  carrot += 1;
+} else if (randomProb == 3) {
   console.log(`${Animal._name} found catfood!`);
-} if else (randomProb == 4) {
-  Animal._bored + 20;
+  catFood += 1;
+} else if (randomProb == 4) {
   console.log(`It's raining and ${Animal._name} can't play alone outside and so is really bored. Boredom increased.`)
-} if else (randomProb == 5) {
-  Animal._thirst + 20;
+  Animal._bored + 20;
+} else if (randomProb == 5) {
   console.log(`That last meal was super salty! ${Animal._name} is more thirsty`);
-} if else (randomProb == 6) {
-  Animal._hunger + 20;
+  Animal._thirst + 20;
+} else if (randomProb == 6) {
   console.log(`${Animal._name} is being greedy today and wants more food. Hunger increased.`)
-} if else (randomProb == 7) {
-  Animal._sleepy + 20;
+  Animal._hunger + 20;
+} else if (randomProb == 7) {
   console.log(`${Animal._name} woke up on the wrong side of the bed this morning. Need for sleep increased.`)
-} if else (randomProb == 8) {
+  Animal._sleepy + 20;
+} else if (randomProb == 8) {
+  console.log(`What a beautiful day! ${Animal._name} is in a good mood and feeling a little less demanding.`)
   Animal._sleepy - 10;
   Animal._hunger - 10;
   Animal._thirst - 10;
   Animal._bored - 10;
-  console.log(`What a beautiful day! ${Animal._name} is in a good mood and feeling a little less demanding.`)
 } else {
-  console.log("It's a new day."}
+  console.log("Today seems pretty normal..."}
 }}
 
 // GAME OVER CONDITIONS
-if (Animal._hunger >= maxHunger) {
-  return console.log(`${Animal.name} has gotten too hungry and ran away`);
+if (Animal.hunger >= maxHunger) {
+  console.log(`${Animal.name} has gotten too hungry and ran away`);
 } else if (Animal._hunger >= alertHung) {
   console.log(`Feed ${Animal.name} immediately!`);
 
 } else if (Animal._thirst >= maxThirst) {
-  return console.log(`${Animal.name} has gotten too thirsty and ran away`);
+  console.log(`${Animal._name} has gotten too thirsty and ran away`);
 } else if (Animal._thirst >= alertThirst) {
-  console.log(`Give ${Animal.name} a drink immediately!`);
+  console.log(`Give ${Animal._name} a drink immediately!`);
 
 } else if (Animal._bored >= maxBored) {
-  return console.log(`${Animal.name} has gotten too bored and ran away`);
+  console.log(`${Animal._name} has gotten too bored and ran away`);
 } else if (Animal._bored >= alertBored) {
-  console.log(`Have fun with ${Animal.name} immediately!`);
+  console.log(`Have fun with ${Animal._name} immediately!`);
   
 } else if (Animal._sleepy >= maxSleepy) {
-  return console.log(`${Animal.name} has gotten too bored and ran away`);
+  console.log(`${Animal._name} has gotten too bored and ran away`);
 } else if (Animal._sleepy >= alertSleepy) {
-  console.log(`Put ${Animal.name} to bed immediately!`);
+  console.log(`Put ${Animal._name} to bed immediately!`);
 }
 
 //START GAME
@@ -173,4 +198,10 @@ const Rosie = new Bunny("Rosie", true);
 
 console.log("Great choice! Now you must take care of your pet, if you don't, they'll run away".)
 
-random
+//Day mechanism
+function startDay () {
+  console.log("It's a new day.")
+  randomEvent();
+  getStats(Animal);
+// create 3 turn  mechanism? when reaching 0, day end and function repeats
+}
